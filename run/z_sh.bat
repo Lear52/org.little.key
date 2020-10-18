c1c2d1c4a6d07a3626b5b42c0b5adddf1542d9a5
Lear52 <35287237+Lear52@users.noreply.github.com>
2020-10-18 10:26:59 +0300

    20201018c

--- run/z_sh.bat
+++ run/z_sh.bat
@@ -1,5 +1,5 @@
 @echo off
+echo cer crl csr 
 java -cp little-key-1.0.0-SNAPSHOT-little-shade.jar  -Duser.region=US  -Dencoding=Cp866 -Dfile.encoding=UTF8  org.little.key.kMessageX509 CERT_DER.cer 
-java -cp little-key-1.0.0-SNAPSHOT-little-shade.jar  -Duser.region=US  -Dencoding=Cp866 -Dfile.encoding=UTF8  org.little.key.kMessageX509 CRL_DER.crl 
-java -cp little-key-1.0.0-SNAPSHOT-little-shade.jar  -Duser.region=US  -Dencoding=Cp866 -Dfile.encoding=UTF8  org.little.key.kMessageX509 CSR_DER.csr 
-
+java -cp little-key-1.0.0-SNAPSHOT-little-shade.jar  -Duser.region=US  -Dencoding=Cp866 -Dfile.encoding=UTF8  org.little.key.kMessageX509  CRL_DER.crl 
+java -cp little-key-1.0.0-SNAPSHOT-little-shade.jar  -Duser.region=US  -Dencoding=Cp866 -Dfile.encoding=UTF8  org.little.key.kMessageX509  CSR_DER.csr 
